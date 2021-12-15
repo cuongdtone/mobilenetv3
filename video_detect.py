911efde6e51f3492fd7be6c9d6010acfc3859e22
Cuong <cuongtran73d1@gmail.com>
2021-12-15 16:04:57 +0700

    upload code

---
+++ video_detect.py
@@ -0,0 +1,22 @@
+
+import cv2
+import numpy as np
+from utils import Face_Detection, Face_Mask, draw
+
+
+face_detect = Face_Mask(model_face='models/det_10g.onnx', model_mask='models/classifier.h5')
+input_path = 0#'/home/cuong/Desktop/Speaking/GreenGlobal/Tram_Chinh_Dien.mp4'
+path_save_video = 'video.mp4'
+cap = cv2.VideoCapture(input_path)
+
+while True:
+    ret, image = cap.read()
+    if ret:
+        boxes, label = face_detect.get(image)
+        image = draw(image, boxes, label)
+        cv2.imshow('img', image)
+        cv2.waitKey(5)
+    else:
+        break
+        print('het')
+plt.show()
