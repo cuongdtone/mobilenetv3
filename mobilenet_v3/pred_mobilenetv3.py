b2c5299470ab974adf04d8ae86e7335a73a03d22
Cuong <cuongtran73d1@gmail.com>
2021-12-15 15:48:43 +0700

    upload code

--- mobilenet_v3/pred_mobilenetv3.py
+++ mobilenet_v3/pred_mobilenetv3.py
@@ -7,8 +7,8 @@ import cv2
 
 
 class mobilenetv3:
-    def __init__(self, weights):
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    def __init__(self, weights, use_cuda=False):
+        self.device = torch.device('cuda:0' if use_cuda else 'cpu')
         self.model = models.mobilenet_v3_small(pretrained=False).to(self.device)
         self.model.classifier = nn.Sequential(
             nn.Linear(576, 128),
@@ -18,15 +18,11 @@ class mobilenetv3:
         self.weights = weights
         self.load_model()
     def load_model(self):
-       self.model.load_state_dict(torch.load(self.weights))
+        self.model.load_state_dict(torch.load(self.weights, map_location=self.device))
     def preprocess_image(self, image):
-        roi = [[6, 19], [45, 84]]
-        image = cv2.resize(image, (160, 100))
-        image = cv2.rectangle(image, roi[0], roi[1], (0, 0, 0), -1)
-        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        img = Image.fromarray(img)
+        img = Image.fromarray(image)
         # img = Image.open(i).convert('RGB')
-        input_size = 224
+        input_size = 112
         normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                          std=[0.229, 0.224, 0.225])
         preprocess = transforms.Compose([
@@ -38,13 +34,10 @@ class mobilenetv3:
         batch_img_tensor = torch.unsqueeze(img_preprocessed, 0)
         return batch_img_tensor
     def predict(self, image):
-        img = self.preprocess_image(image)
+        img = self.preprocess_image(image).to(self.device)
         self.model.eval()
         out = self.model(img)
         _, index = torch.max(out, 1)
-        percentage = (nn.functional.softmax(out, dim=1)[0] * 100).tolist()
-        if index[0] == 0:
-            return 'thehdv', float(percentage[0])
-        else:
-            return 'thekhac', percentage[1]
+        #percentage = (nn.functional.softmax(out, dim=1)[0]).tolist()
+        return index.numpy()[0]
 
